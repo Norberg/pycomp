947381ca02e48e61c02da3b6eeb47e891c94cdf5
Simon Norberg <simon@pthread.se>
2011-12-10 14:57:26 +0100

    acitivated llvm optimization

--- compile.py
+++ compile.py
@@ -2,16 +2,23 @@
 import ast, sys, os
 import translator
 filename = sys.argv[1]
-llfile = filename.strip(".py")+".ll"
+binfile=filename.strip(".py")
+llfile = binfile+".ll"
 visitor = translator.MyVisitor(llfile)
 fp = open(filename)
 sourcecode = fp.read() 
 a = ast.parse(sourcecode, filename)
 visitor.visit(a)
 visitor.close()
-bcfile = filename.strip(".py")+".bc"
-sfile = filename.strip(".py")+".s"
-os.system("llvm-as -f {}".format(llfile))
-os.system("llc -f {}".format(bcfile))
-os.system("gcc {}".format(sfile))
-os.system("rm {} {}".format(bcfile, sfile))
+bcfile = binfile+".bc"
+optfile = binfile+".opt"
+sfile = binfile+".s"
+os.system("llvm-as {}".format(llfile))
+os.system("opt -mem2reg {} > {}".format(bcfile,optfile))
+os.system("mv {} {}".format(optfile,bcfile))
+os.system("llc {}".format(bcfile))
+os.system("gcc {} -o {}".format(sfile, binfile))
+os.system("mkdir debug 2>/dev/null")
+for f in(bcfile, sfile, llfile):
+	os.system("mv {} debug/".format(f))
+#os.system("rm {} {} {}".format(bcfile, sfile, llfile))
--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import difflib, glob, os, sys
 
 for test in glob.glob("test/*.py"):
 	failed = False
+	binfile = test.strip(".py")
 	os.system("python3 {} > test/output.reference".format(test))
 	os.system("./compile.py {}".format(test))
-	os.system("./a.out > test/output")
+	os.system("./{} > test/output".format(binfile))
 	diff = difflib.context_diff(open("test/output.reference").readlines(),
 	       open("test/output").readlines(), "reference", "testcase")
 	for d in diff:
@@ -15,3 +16,7 @@ for test in glob.glob("test/*.py"):
 		print(d, end='')
 	if not failed:
 		print("Testcase: {} succeeded!".format(test))
+	#cleanup
+	os.system("rm {}".format(binfile))
+	os.system("rm test/output")
+
